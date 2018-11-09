c7b3db71e4b8eda1ece3cf2ee3afdaf21870bdff
zhiqunTang <tang.zhiqun@jancar.com.cn>
2018-11-09 14:32:37 +0800

    feat:添加leakcanary,blockcanary

--- Bluetooth/app/src/main/java/com/jancar/bluetooth/phone/BluetoothApplication.java
+++ Bluetooth/app/src/main/java/com/jancar/bluetooth/phone/BluetoothApplication.java
@@ -2,17 +2,25 @@ package com.jancar.bluetooth.phone;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.multidex.MultiDex;
 import android.util.Log;
 import android.view.ViewConfiguration;
 
+import com.github.moduth.blockcanary.BlockCanary;
+import com.github.moduth.blockcanary.BlockCanaryContext;
 import com.jancar.bluetooth.lib.BluetoothManager;
 import com.jancar.bluetooth.phone.util.FlyLog;
 import com.orhanobut.hawk.Hawk;
 import com.orhanobut.hawk.HawkBuilder;
 import com.orhanobut.hawk.LogLevel;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import java.lang.reflect.Field;
+
+import static android.security.keystore.SoterUtil.TAG;
 
 /**
  * @anthor Tzq
@@ -20,11 +28,26 @@ import java.lang.reflect.Field;
  * @describe application
  */
 public class BluetoothApplication extends Application {
+    private static final String TAG = "BluetoothApplication";
+    private RefWatcher refWatcher;
+
+    private RefWatcher setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return RefWatcher.DISABLED;
+        }
+        return LeakCanary.install(this);
+    }
+
+    public static RefWatcher getRefWatcher(Context context) {
+        BluetoothApplication leakApplication = (BluetoothApplication) context.getApplicationContext();
+        return leakApplication.refWatcher;
+    }
 
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.e(TAG, "onCreate===");
         BluetoothManager.getBluetoothManagerInstance(this);
         /**
          * 设置Marquee不显示省略号
@@ -43,6 +66,8 @@ public class BluetoothApplication extends Application {
                 .setStorage(HawkBuilder.newSqliteStorage(this))
                 .setLogLevel(LogLevel.FULL)
                 .build();
+        BlockCanary.install(this, new BlockCanaryContext()).start();
+        refWatcher = setupLeakCanary();
     }
 
     @Override
@@ -61,6 +86,8 @@ public class BluetoothApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+        Log.e(TAG, "attachBaseContext===");
         MultiDex.install(this);
     }
+
 }
