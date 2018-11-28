bf396a285d0e86083ff801e7bce911b61ae9afe4
zhiqunTang <tang.zhiqun@jancar.com.cn>
2018-11-28 10:55:07 +0800

    del:删除多余文件

--- Bluetooth/app/src/main/java/com/jancar/bluetooth/phone/listener/ILettersSection.java
+++
@@ -1,9 +0,0 @@
-package com.jancar.bluetooth.phone.listener;
-
-public interface ILettersSection {
-	
-	/**
-	 * 根据分类的首字母的Char ascii值获取其第一次出现该首字母的位置
-	 */
-    int getPositionForSection(int section) ;
-}
