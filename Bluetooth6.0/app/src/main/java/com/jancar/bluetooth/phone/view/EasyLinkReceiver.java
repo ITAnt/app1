96afaf74e30c1c44467c7a2a1b5234c0c0377b89
zhiqunTang <tang.zhiqun@jancar.com.cn>
2018-12-10 15:51:31 +0800

    fix:修改亿联改为暂时不支持后台播放

--- Bluetooth6.0/app/src/main/java/com/jancar/bluetooth/phone/view/EasyLinkReceiver.java
+++ Bluetooth6.0/app/src/main/java/com/jancar/bluetooth/phone/view/EasyLinkReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 
+import com.jancar.bluetooth.lib.BluetoothManager;
 import com.jancar.bluetooth.phone.util.Constants;
 
 /**
@@ -15,12 +16,17 @@ import com.jancar.bluetooth.phone.util.Constants;
  */
 public class EasyLinkReceiver extends BroadcastReceiver {
     private static final String TAG = "EasyLinkReceiver";
-    private static final String EASY_GET_FOUS = "net.easyconn.a2dp.acquire";
-    private static final String EASY_LOSS_FOUS = "net.easyconn.a2dp.release";
+    private static final String EASY_GET_FOUS = "net.easyconn.a2dp.acquire";//获取蓝牙声道（进入投屏时）
+    private static final String EASY_LOSS_FOUS = "net.easyconn.a2dp.release";//释放蓝牙声道（退出 投屏时
     private static final String BOOT_COMPLETE = "android.intent.action.BOOT_COMPLETED";
     private static final String BECOMING_NOISY = "android.media.AUDIO_BECOMING_NOISY";
     private static final String ACTION_BT_STATE_CHANGE = "com.jancar.bt.state.change";
     private static final String PREBOOT_IPO = "android.intent.action.ACTION_PREBOOT_IPO";
+    private static final String EASY_BLUETHOTH_CONNECTED = "net.easyconn.bt.connected";//蓝牙连接
+    private static final String EASY_BLUETHOTH_DISCONNECTED = "net.easyconn.bt.notconnected ";//蓝牙断开
+    private static final String EASY_EXIT_ANDRID = "net.easyconn.android.mirror.out";//退出 Android 投屏
+    private static final String EASY_EXIT_ANDROID_BACK = "net.easyconn.screen.pause";//退出Android投屏，进入后台
+    private static final String EASY_EXIT_IPHONE = "net.easyconn.iphone.inner.mirror.out";//退出iphone投屏
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -44,13 +50,32 @@ public class EasyLinkReceiver extends BroadcastReceiver {
 //            bluetoothRequestFocus.releaseAudioFocus();
 
         } else if (action.equals(EASY_GET_FOUS)) {
-            //BluetoothManager.getBluetoothManagerInstance(context).setPlayerState(true);
+//                BluetoothManager.getBluetoothManagerInstance(context).setPlayerState(true);
             if (!bluetoothRequestFocus.isNeedGainFocus()) {
+                if (!bluetoothRequestFocus.getPlayStatus()) {
+                    bluetoothRequestFocus.setBTPlayStatus(true);
+                }
                 bluetoothRequestFocus.requestAudioFocus();
             }
             if (!bluetoothRequestFocus.isBTConnect()) {
 //                Toast.makeText(context, "连接蓝牙之后才能在车机上听到声音", Toast.LENGTH_SHORT).show();
             }
+        } else if (action.equals(EASY_BLUETHOTH_CONNECTED)) {
+            //蓝牙连接
+        } else if (action.equals(EASY_BLUETHOTH_DISCONNECTED)) {
+            //蓝牙断开
+        } else if (action.equals(EASY_EXIT_ANDRID)) {
+            //退出安卓投屏
+            bluetoothRequestFocus.setBTPlayStatus(false);
+            bluetoothRequestFocus.releaseAudioFocus();
+        } else if (action.equals(EASY_EXIT_IPHONE)) {
+            //iphone退出投屏
+            bluetoothRequestFocus.setBTPlayStatus(false);
+            bluetoothRequestFocus.releaseAudioFocus();
+        } else if (action.equals(EASY_EXIT_ANDROID_BACK)) {
+            //退出Android投屏，进入后台
+            bluetoothRequestFocus.setBTPlayStatus(false);
+            bluetoothRequestFocus.releaseAudioFocus();
         }
     }
 }
